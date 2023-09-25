b92dd83b36f3b91bc32967e434825a97f94a1cf5
Alex Peterson <aspeterson.126@gmail.com>
2023-09-25 10:27:20 -0500

    Refactor App.js to invoke getUrls fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+// import UrlForm from '../UrlForm/UrlForm';
 
 function App () {
   const [urls, setUrls] = useState([]);
 
   useEffect(() => {
-
-  })
+    getUrls()
+    .then(data => {
+      console.log(data.urls)
+      setUrls(data.urls)
+    }) .catch(error => console.log(error.message))
+  }, [])
 
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        {/* <UrlForm /> */}
       </header>
 
-      <UrlContainer urls={"<<<Urls should go here>>>"}/>
+      <UrlContainer urls={urls}/>
     </main>
   );
 }
